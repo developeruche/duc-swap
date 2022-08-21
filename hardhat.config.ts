c40235da40995350a7aeef2bbdad38947ae2f7fe
Nwele Uchenna <nweleuchenna2004@gmail.com>
2022-08-22 00:22:16 +0100

    done working on the swap (UNISWAP fork ====> )

---
+++ hardhat.config.ts
@@ -0,0 +1,24 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.5.16"
+      },
+      {
+        version: "0.6.6"
+      },
+      {
+        version: "0.8.9"
+      },
+      {
+        version: "0.8.0"
+      }
+    ]
+  },
+  
+};
+
+export default config;
